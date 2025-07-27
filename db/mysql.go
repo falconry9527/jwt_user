e7a66b9c2bfaea5c0540d16a263874de21f9b13e
conry <kequan888@gmail.com>
2025-07-27 16:03:28 +0800

    fix

--- db/mysql.go
+++ db/mysql.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 // InitDB 初始化数据库连接
 func InitDB() {
 	var err error
-	dsn := "root:Conry@1238@tcp(127.0.0.1:3306)/jwt?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:ZAqbXlUeg0nnmWRXzyLbhSc9AXuZh4m9@tcp(127.0.0.1:3306)/jwt?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
